c59ed30f3716620cff4f31e9a4b967308fc5fda8
Shaveen Bageloo <shaveen.bageloo@standardbank.co.za>
2019-10-17 22:29:02 +0200

    Text box now querying TMDB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,25 +15,28 @@ export class AppComponent {
 
   todoArray = [];
 
-  searchMovies(searchBar: string) {
-    axios.get('https://api.themoviedb.org/3/search/movie',
-      {
+  searchMovies(searchBar) {
+
+    axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: {
         api_key: '7d46a1e2eaad722e50de334a6895d840',
         language: 'en-US',
         query: searchBar,
         page: '1',
         include_adult: 'false'
       }
-    ).then(response => {
+    }).then(response => {
+
       let jsonObject = JSON.parse(JSON.stringify(response.data.results));
       jsonObject.forEach(element => {
         // console.log('Name of the movies : ' + element.title);
         this.todoArray.push(element.title);
       });
-    })
-      .catch(e => {
-        console.log('Error has occured ' + e);
-      })
+
+    }).catch(e => {
+      console.log('Error has occured ' + e);
+    });
+
   }
 
   addTodo(value) { this.todoArray.push(value); console.log(this.todoArray); };
