f1dff456520b285e0c962374d3043d5977ece431
Shaveen Bageloo <shaveen.bageloo@standardbank.co.za>
2019-10-17 22:19:57 +0200

    Reading from a TMDB using axios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
   title = 'netflix-mini-web-app';
 
   todoArray = [];
+
+  searchMovies(searchBar: string) {
+    axios.get('https://api.themoviedb.org/3/search/movie',
+      {
+        api_key: '7d46a1e2eaad722e50de334a6895d840',
+        language: 'en-US',
+        query: searchBar,
+        page: '1',
+        include_adult: 'false'
+      }
+    ).then(response => {
+      let jsonObject = JSON.parse(JSON.stringify(response.data.results));
+      jsonObject.forEach(element => {
+        // console.log('Name of the movies : ' + element.title);
+        this.todoArray.push(element.title);
+      });
+    })
+      .catch(e => {
+        console.log('Error has occured ' + e);
+      })
+  }
 
   addTodo(value) { this.todoArray.push(value); console.log(this.todoArray); };
 
