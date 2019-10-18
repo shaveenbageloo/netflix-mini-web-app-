d3a37137c7ad6d8266a75984537384bef5bb36e9
Shaveen Bageloo <shaveen.bageloo@standardbank.co.za>
2019-10-18 23:59:09 +0200

    Working on getting the favs from the session store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,34 @@ export class AppComponent {
   title = 'netflix-mini-web-app';
 
   todoArray = [];
+  favArray = [];
+  essArray = [];
+
+
+  getPostPath(inPath) {
+    let posterPath = '';
+
+    if (inPath) {
+      if (inPath.indexOf('https://image.tmdb.org/t/p/w500') > 0) {
+        posterPath = inPath;
+      }
+      else {
+        posterPath = 'https://image.tmdb.org/t/p/w500' + inPath;
+      }
+    } else {
+      posterPath = 'https://thumbs.dreamstime.com/z/no-user-profile-picture-24185395.jpg';
+    }
+    return posterPath;
+  }
+
 
 
   constructor(private _cookieService: CookieService) { }
 
   ngOnInit() {
     this._cookieService.put("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+
+
 
     axios.get('https://api.themoviedb.org/3/discover/movie', {
       params: {
@@ -37,17 +59,10 @@ export class AppComponent {
     }).then(response => {
 
       const jsonObject = JSON.parse(JSON.stringify(response.data.results));
-      jsonObject.forEach(element => {
-        let posterPath = '';
 
-        console.log('poster_path of the movies : ' + element.poster_path + ' - ' + element.release_date);
-        if (element.poster_path) {
-          posterPath = 'https://image.tmdb.org/t/p/w500' + element.poster_path;
-        }
-        else {
-          posterPath = 'https://thumbs.dreamstime.com/z/no-user-profile-picture-24185395.jpg';
-        }
-        const movieObject = new Movies(element.title, element.release_date, element.vote_average, posterPath);
+      jsonObject.forEach(element => {
+
+        const movieObject = new Movies(element.title, element.release_date, element.vote_average, this.getPostPath(element.poster_path));
         this.todoArray.push(movieObject);
 
       });
@@ -56,46 +71,66 @@ export class AppComponent {
       console.log('Error has occured ' + e);
     });
   }
+
+
+
+
+
 
   searchMovies(searchBar) {
 
     this.todoArray = [];
 
-    axios.get('https://api.themoviedb.org/3/search/movie', {
-      params: {
-        api_key: '7d46a1e2eaad722e50de334a6895d840',
-        language: 'en-US',
-        query: searchBar,
-        page: '1',
-        include_adult: 'false'
-      }
-    }).then(response => {
+    if (searchBar === '#showFavs#') {
+      console.log('Searching on your favs for now');
 
-      const jsonObject = JSON.parse(JSON.stringify(response.data.results));
-      jsonObject.forEach(element => {
-        let posterPath = '';
+      const checkSessionStore = (sessionStorage.getItem('userFavourites'));
+      const JSONSS = JSON.parse(JSON.stringify( checkSessionStore));
 
-        // console.log('poster_path of the movies : ' + element.poster_path + ' - ' + element.release_date);
-        if (element.poster_path) {
-          posterPath = 'https://image.tmdb.org/t/p/w500' + element.poster_path;
-        }
-        else {
-          posterPath = 'https://thumbs.dreamstime.com/z/no-user-profile-picture-24185395.jpg';
-        }
-        const movieObject = new Movies(element.title, element.release_date, element.vote_average, posterPath);
+      JSONSS.forEach(element => {
+
+        const e = JSON.parse(element);
+        const movieObject = new Movies(e.title, e.releaseDate, e.voteAverage, e.posterPath);
         this.todoArray.push(movieObject);
 
       });
 
-    }).catch(e => {
-      console.log('Error has occured ' + e);
-    });
+    } else {
+
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '7d46a1e2eaad722e50de334a6895d840',
+          language: 'en-US',
+          query: searchBar,
+          page: '1',
+          include_adult: 'false'
+        }
+      }).then(response => {
+
+        const jsonObject = JSON.parse(JSON.stringify(response.data.results));
+        jsonObject.forEach(element => {
+
+          const movieObject = new Movies(element.title, element.release_date, element.vote_average, this.getPostPath(element.poster_path));
+          this.todoArray.push(movieObject);
+
+
+        });
+
+      }).catch(e => {
+        console.log('Error has occured ' + e);
+      });
+
+    }
 
   }
 
-  addTodo(value) { this.todoArray.push(value); console.log(this.todoArray); };
+  addTodo(value) {
+    const valueToStore = (JSON.stringify(value));
+    this.favArray.push(valueToStore);
+    sessionStorage.setItem('userFavourites', valueToStore);
+  };
 
-  addtoFav(todo) {
+  deleteToDo(todo) {
     for (let i = 0; i <= this.todoArray.length; i++) {
       if (todo === this.todoArray[i]) {
         this.todoArray.splice(i, 1);
